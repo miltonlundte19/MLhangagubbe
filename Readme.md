bcf529a45893801bfe4b2e4e4f1fdfb43de4022f
miltonlundte19 <milton.lund@elev.ga.ntig.se>
2021-01-15 11:16:49 +0100

    gjorde redmi och berjar med mord veljaren.

---
+++ Readme.md
@@ -0,0 +1,5 @@
+
+
+- 2021-01-15
+Började med att försöka gjöra en ord veljare (men jag har misat maser med saker)
+
---
+++ src/hangagubbe.java
@@ -0,0 +1,12 @@
+public class hangagubbe {
+    public static void main(String[] args) {
+        String ord = ordslumpare();
+        
+    }
+
+    private static String ordslumpare() {
+        String[] ordind = {"ett", "java", };
+
+        return null;
+    }
+}
