0d6359afc1a6cba51b4101b251c81e13745d1804
miltonlundte19 <milton.lund@elev.ga.ntig.se>
2021-01-19 11:00:28 +0100

    gjor så att jag får ordets längd

--- src/hangagubbe.java
+++ src/hangagubbe.java
@@ -1,6 +1,12 @@
+import javax.swing.*;
+
 public class hangagubbe {
     public static void main(String[] args) {
         String ord = ordslumpare();
+        int ordlän = ord.length();
+        String blanord = "_";
+        int k;
+        System.out.println(ordlän);
 
     }
 
